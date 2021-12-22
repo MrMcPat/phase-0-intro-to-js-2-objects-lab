236311026bde5ea901bf0c6400f57545c8cf28ff
Patrick Liang <pl204511194@yahoo.com>
2021-12-22 13:29:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Pat",
+    streetAddress: "123 4th Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
